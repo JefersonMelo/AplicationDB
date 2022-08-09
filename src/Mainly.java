ffe72e78e72f655a8486e38638b70820dcaaeca9
Jeferson Melo <jefsdemelo@gmail.com>
2022-08-09 20:22:07 -0300

    Design da aba de login e logica de mudança de aba para conectado concluídos

--- src/Mainly.java
+++ src/Mainly.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -14,6 +17,8 @@ public class Mainly extends javax.swing.JFrame {
      */
     public Mainly() {
         initComponents();
+
+        abas.setEnabledAt(1, false);
     }
 
     /**
@@ -25,11 +30,88 @@ public class Mainly extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        abas = new javax.swing.JTabbedPane();
+        abaLogin = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        btnLogin = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        txtUsuario = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        txtSenha = new javax.swing.JPasswordField();
+        abaCadastro = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
+        abaLogin.setLayout(null);
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.setLayout(null);
+
+        btnLogin.setText("Conectar");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnLogin);
+        btnLogin.setBounds(369, 220, 90, 28);
+
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("Senha");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(10, 100, 480, 20);
+
+        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtUsuario);
+        txtUsuario.setBounds(10, 50, 460, 28);
+
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Usuário");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(10, 26, 460, 20);
+        jPanel1.add(txtSenha);
+        txtSenha.setBounds(10, 120, 460, 28);
+
+        abaLogin.add(jPanel1);
+        jPanel1.setBounds(50, 40, 480, 260);
+
+        abas.addTab("Login", abaLogin);
+
+        abaCadastro.setLayout(null);
+        abas.addTab("Cadastro", abaCadastro);
+
+        getContentPane().add(abas);
+        abas.setBounds(0, 20, 580, 440);
+
         setBounds(0, 0, 594, 495);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUsuarioActionPerformed
+
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        // TODO add your handling code here:
+
+        String senha = new String(txtSenha.getPassword());
+
+        if (txtUsuario.getText().equals("adm") && senha.equals("adm")) {
+            abas.setEnabledAt(1, true);
+            abas.setSelectedIndex(1);
+        }else{
+            JOptionPane.showMessageDialog(
+                    null, 
+                    "Usuário ou Senha Não Conferem",
+                    "Mensagem", 
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        }
+    }//GEN-LAST:event_btnLoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -67,5 +149,14 @@ public class Mainly extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel abaCadastro;
+    private javax.swing.JPanel abaLogin;
+    private javax.swing.JTabbedPane abas;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField txtSenha;
+    private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
